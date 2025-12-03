6d0f2f4b344ba2e0e9a6b117eaaf1301ed32adf6
gaeunee2 <gaeunee2>
2025-12-04 03:28:46 +0900

    #Scene2 개발 완료

---
+++ Assets/Scripts/TitleUIController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleUIController : MonoBehaviour
+{
+    [SerializeField] private Button firstPersonButton;
+    [SerializeField] private Button thirdPersonButton;
+
+    private void Start()
+    {
+        firstPersonButton.onClick.AddListener(OnClickFirstPerson);
+        thirdPersonButton.onClick.AddListener(OnClickThirdPerson);
+    }
+
+    private void OnClickFirstPerson()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.CurrentViewMode = ViewMode.FirstPerson;
+        }
+
+        SceneManager.LoadScene("3_GameScene1");
+    }
+
+    private void OnClickThirdPerson()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.CurrentViewMode = ViewMode.ThirdPerson;
+        }
+
+        SceneManager.LoadScene("GameScene1_Ground");
+    }
+}
