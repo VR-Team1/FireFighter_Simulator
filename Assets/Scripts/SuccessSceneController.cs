02bc5f5b8d03c820df6c3cc0c5d40b14ea3ff432
Naongjin <syje1004@naver.com>
2025-12-08 05:23:12 +0900

    씬6 - 처음으로 버튼
    씬 로드 안되는 문제 해결

--- Assets/Scripts/SuccessSceneController.cs
+++ Assets/Scripts/SuccessSceneController.cs
@@ -3,7 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class SuccessSceneController : MonoBehaviour
 {
-    public void OnClickGoToTitle()
+    public void Update()
+    {
+        
+    }
+    public void OnClick()
     {
         SceneManager.LoadScene("2_TitleScene");
     }
