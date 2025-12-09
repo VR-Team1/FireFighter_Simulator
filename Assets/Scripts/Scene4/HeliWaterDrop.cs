b0dcf5c15ba99c769410ae120a4f4f0e70494362
gaeunee2 <gaeunee2>
2025-12-10 02:13:32 +0900

    Scene4 waterAudio 추가

--- Assets/Scripts/Scene4/HeliWaterDrop.cs
+++ Assets/Scripts/Scene4/HeliWaterDrop.cs
@@ -2,32 +2,78 @@ using UnityEngine;
 
 public class HeliWaterDrop : MonoBehaviour
 {
+    [Header("물 파티클")]
     public ParticleSystem waterParticle;
+
+    [Header("물 관리 매니저")]
     public WaterManager waterManager;
+
+    [Header("물 떨어질 때 나는 소리")]
+    public AudioSource waterAudio;
+
+    private bool isDropping = false;
 
     void Start()
     {
-        var emission = waterParticle.emission;
-        emission.enabled = false;
+
+        if (waterParticle != null)
+        {
+            var emission = waterParticle.emission;
+            emission.enabled = false;
+        }
+
+        if (waterAudio != null)
+        {
+            waterAudio.loop = true; 
+            waterAudio.Stop();
+        }
     }
 
     void Update()
     {
-        bool hasWater = waterManager.GetWaterPercent() > 0f;
-        bool isOutsideWater = waterManager.isInsidePlane == false;
+        bool hasWater = waterManager != null && waterManager.GetWaterPercent() > 0f;
+        bool isOutsideWater = waterManager != null && waterManager.isInsidePlane == false;
 
-        if (Input.GetKey(KeyCode.E) && hasWater && isOutsideWater)
+        bool shouldDrop = Input.GetKey(KeyCode.E) && hasWater && isOutsideWater;
+
+        if (shouldDrop && !isDropping)
+        {
+            StartDrop();
+        }
+        else if (!shouldDrop && isDropping)
+        {
+            StopDrop();
+        }
+    }
+
+    void StartDrop()
+    {
+        isDropping = true;
+
+        if (waterParticle != null)
         {
             var emission = waterParticle.emission;
             emission.enabled = true;
-
-            // 원하면 물 감소도 추가 가능
-            // waterManager.UseWater(Time.deltaTime * 5f);
         }
-        else
+        if (waterAudio != null && !waterAudio.isPlaying)
+        {
+            waterAudio.Play();
+        }
+    }
+
+    void StopDrop()
+    {
+        isDropping = false;
+
+        if (waterParticle != null)
         {
             var emission = waterParticle.emission;
             emission.enabled = false;
+        }
+
+        if (waterAudio != null && waterAudio.isPlaying)
+        {
+            waterAudio.Stop();
         }
     }
 }
